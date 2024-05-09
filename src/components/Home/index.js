934c7b040f900764509d7a2ff7b7685064f8ca46
Veena2991 <veenabattul@gmail.com>
2024-05-09 13:05:13 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const Home = () => (
+  <div className="body-div">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="images"
+    />
+    <h1 className="heading">Home</h1>
+  </div>
+)
+
+export default Home
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="body-div">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not found"
+      className="images"
+    />
+    <h1 className="heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
